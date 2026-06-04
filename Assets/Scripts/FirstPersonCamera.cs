5195fdb27327022d0b6cf9865673175e42cf9fe3
gartnerfn <gartnerfn@gmail.com>
2026-06-04 19:04:56 -0300

    feat(FPSCamera): able to go up and down

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class FirstPersonCamera : MonoBehaviour
 {
     public float moveSpeed = 200f;
-    public float sprintSpeed = 10f;
     public float mouseSensitivity = 150f;
+    public float initialYaw = 180f;
 
     private float pitch = 0f;
     private float yaw = 0f;
 
     void Start()
     {
+        Vector3 angles = transform.eulerAngles;
+
+        pitch = angles.x;
+        yaw = initialYaw;
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -29,14 +35,16 @@ public class FirstPersonCamera : MonoBehaviour
         //
         // Movimiento
         //
-        float speed = Input.GetKey(KeyCode.LeftShift)
-            ? sprintSpeed
-            : moveSpeed;
-
         Vector3 move =
             transform.forward * Input.GetAxis("Vertical") +
             transform.right * Input.GetAxis("Horizontal");
 
-        transform.position += move * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+            move += Vector3.up;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            move += Vector3.down;
+
+        transform.position += move * moveSpeed * Time.deltaTime;
     }
 }
